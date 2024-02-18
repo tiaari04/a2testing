8b4569d5fad55efe6077e7b4892bc1f43138ee0f
tiaari04 <150181059+tiaari04@users.noreply.github.com>
2024-02-18 16:24:41 -0700

    Create q1.sql

---
+++ q1.sql
@@ -0,0 +1,3 @@
+SELECT DISTINCT members.name, email
+FROM waitlists, members, borrowings
+WHERE borrowings.member = waitlists.member AND waitlists.member = members.email AND borrowings.book_id = waitlists.book_id;
