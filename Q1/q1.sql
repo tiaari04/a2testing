174e1a8d4515ce8d4759d4cbd366d50ab9e548a4
tiaari04 <150181059+tiaari04@users.noreply.github.com>
2024-02-18 19:49:19 -0700

    Update q1.sql

--- Q1/q1.sql
+++ Q1/q1.sql
@@ -1,3 +1,4 @@
 SELECT DISTINCT members.name, email
 FROM waitlists, members, borrowings
 WHERE borrowings.member = waitlists.member AND waitlists.member = members.email AND borrowings.book_id = waitlists.book_id;
+
